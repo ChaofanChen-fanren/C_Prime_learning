790684a9b3a960da53409412e590508ef07a5941
ccf <2536831142@qq.com>
2022-10-17 22:26:32 +0800

    Web search keyword

--- python_Web/Project/myadmin/urls.py
+++ python_Web/Project/myadmin/urls.py
@@ -4,7 +4,7 @@ from .views import index,news
 urlpatterns = [
     path('', index.index,name = "myadmin_index"),
     
-     #员工账号信息管理
+     #新闻爬取后台管理
     path('news/<int:pIndex>', news.index, name="myadmin_news_index"),#浏览信息
     path('news/add', news.add, name="myadmin_news_add"),             #加载添加表单
     path('news/insert', news.insert, name="myadmin_news_insert"),     #执行信息添加
--- python_Web/Project/myadmin/views/news.py
+++ python_Web/Project/myadmin/views/news.py
@@ -6,6 +6,15 @@ def index(request,pIndex=1):
     '''浏览信息'''
     umod = News.objects
     list = umod.all()
+    keyWord_list=[]
+
+
+    # 获取、判断并封装关keyword键搜索
+    kw = request.GET.get("keyword",None)
+    if kw:
+        # 查询员工账号或昵称中只要含有关键字的都可以
+        list = list.filter(Q(title__contains=kw) | Q(content__contains=kw) | Q(school__contains=kw))
+        keyWord_list.append(kw)
 
     #执行分页处理
     pIndex = int(pIndex)
@@ -22,13 +31,21 @@ def index(request,pIndex=1):
     list2 = page.page(pIndex) 
     # 将当前数据的新闻文本的字符串截取前30个字符显示
     for l in list2:
-        l.content = l.content[:30]
+        if kw:
+            kw_index = l.content.find(kw)
+            if kw_index != -1:
+                start = max(0,kw_index-15)
+                l.content = l.content[start:start+30]
+            else:
+                l.content = l.content[:30]
+        else:
+            l.content = l.content[:30]
     
     #页码数列表
     first_page = max(1,pIndex-3);
     plist = range(first_page,first_page + 6) #显示包含此页的页列表
     #封装信息加载模板输出
-    context = {"newslist":list2,'plist':plist,'pIndex':pIndex}
+    context = {"newslist":list2,'plist':plist,'pIndex':pIndex,'kw_list':keyWord_list}
     return render(request,"myadmin/news/index.html",context)
 
 def add(request):
