f19d3349fe5a48ee8d3cdd164173ac23587a327c
ccf <2536831142@qq.com>
2022-10-23 23:38:16 +0800

    News Web finishgit statusgit status!

--- python_Web/Project/web/views.py
+++ python_Web/Project/web/views.py
@@ -1,3 +1,4 @@
+from calendar import month
 from django.shortcuts import render
 from django.http import HttpResponse
 from myadmin.models import News
@@ -29,6 +30,17 @@ def index(request):
         showWorldCould()
     
 
+    # 展示词频
+    import json
+    tf = open("./static/web/photo/myDictionary.json", "r")
+    word_list = json.load(tf)[0:7]
+    tf.close()
+    word_dict = {"word_name":[v['name'] for v in word_list],"word_value":[v['value'] for v in word_list]}
+    # print(word_dict)
+    
+
+    month_value = show_Month_value()
+
     # 展示最近前几篇新闻
     page = Paginator(New_lists,7) #以10条每页创建分页对象
     news_page_1 = page.page(1) #只需要第一页
@@ -36,10 +48,7 @@ def index(request):
     for new in news_page_1:
         new.title = new.title[0:20]
 
-
-        
-
-    context = {"school":school,"newslist":news_page_1}
+    context = {"school":school,"newslist":news_page_1,"word_count":word_dict,'month_value':month_value}
     # context = {"data":{"data":[5, 20, 36, 10, 10, 20],"name":['校庆办', '党委组织部', '党委宣传部', '计算机学院', '社会资源处', '经管学院']}}
     # wordCloud()
     return render(request,'web/index.html',context)
@@ -60,11 +69,34 @@ def showWorldCould():
     text = ""
     for new in New_lists:
         text += new.content
-    # print(str)
-    word_list = jieba.cut(text,cut_all= True)
+    # print(text)
+
+    # 统计词频
+    words = jieba.lcut(text)
+    counts = {}
+    word_list = []
+    for word in words:
+        if len(word) == 1:
+            continue
+        else:
+            counts[word] =counts.get(word,0) + 1
+            word_list.append(word)
+    items = list(counts.items())
+    items.sort(key=lambda x: x[1], reverse=True)
+    wordclout_dict = [{"name": k[0], "value": k[1]} for k in items]
+    
+    #将词频统计存入txt文件
+    import json
+    tf = open("./static/web/photo/myDictionary.json", "w")
+    json.dump(wordclout_dict,tf)
+    tf.close()
+
+    #生成词云图
+    # word_list = jieba.cut(text,cut_all= True)
     result = " ".join(word_list)
-    import os
-    print(os.listdir("./static/web/photo/"))
+    # print(word_list)
+    # import os
+    # print(os.listdir("./static/web/photo/"))
     mask = np.array(image.open("./static/web/photo/WCbg.jpg"))
     wordcloud = WordCloud(
         # 遮罩层,除白色背景外,其余图层全部绘制（之前设置的宽高无效）
@@ -72,6 +104,23 @@ def showWorldCould():
         #默认黑色背景,更改为白色
         background_color='#FFFFFF',
         # 若想生成中文字体,需添加中文字体路径
-        font_path="./static/web/photo//阿里巴巴普惠体 B.ttf"
+        font_path="./static/web/photo/阿里巴巴普惠体 B.ttf"
     ).generate(result)
     wordcloud.to_file("./static/web/photo/new_wordcloud.jpg")
+
+    return wordclout_dict
+
+
+def show_Month_value():
+    import pandas as pd
+    df = pd.read_csv('./static/web/photo/news.csv')
+    # 将日期转化为pandas的 datetime
+    df.set_index(pd.to_datetime(df['发表日期']),inplace=True)
+    # 统计日期发表篇数
+    # print(df)
+    # 得到所有月份
+    month = [str(v) + '月' for v in list(df.groupby(df.index.month)['新闻标题'].count().index)]
+    # 得到所有月份对应的发表篇数
+    value = list(df.groupby(df.index.month)['新闻标题'].count())
+
+    return {'month':month,'value':value}
